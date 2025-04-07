f0969598dc9d7d7abde3bbeb92d91f76b5c4c10c
colinrosati <colin.rosati@commercetools.com>
2025-04-07 23:27:15 +0200

    WIP style Heading

--- app/components/Heading.jsx
+++ app/components/Heading.jsx
@@ -1,20 +1,25 @@
 import { getRandomMember } from "@/sanity/sanity.utils";
 import Image from "next/image";
+import './Heading.css';
 
 export default async function Heading({ headingText }) {
   const [randomMember] = await getRandomMember()
 
   return (
-    <div>
-      {headingText && <h2>{headingText}</h2>}
-
+    <div className="heading-container">
+      {headingText && 
+        <div className="heading-text-container">
+          <h2 className="heading-text">{headingText}</h2>
+        </div>
+      }
       <div className="">
-        <Image src={randomMember.image.asset.url} alt={`${randomMember.image.alt? randomMember.image.alt: 'Room Up Front Member portfolio piece'}`}
-        width={800} height={800}/>
-      <p>{randomMember.name}</p>
-      <p>{randomMember.location}</p>
-    </div>
-
+          <Image src={randomMember.image.asset.url} alt={`${randomMember.image.alt? randomMember.image.alt: 'Room Up Front Member portfolio piece'}`}
+          width={800} height={800}/>
+          <div className="heading-image-text-box">
+            <p className="heading-image-name">{randomMember.name}</p>
+            <p className="heading-image-location">{randomMember.location}</p>
+          </div>
+      </div>
     </div>
   );
 }
