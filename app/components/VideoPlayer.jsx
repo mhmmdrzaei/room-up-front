3265197d17a71d622bfd6dd699b634ef9238ea5e
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2025-06-26 17:32:19 -0700

    video change

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -4,15 +4,32 @@ import dynamic from 'next/dynamic'
 const ReactPlayer = dynamic(() => import('react-player'), { ssr: false })
 
 export default function VideoPlayer({ link }) {
+  // Quick sanity check
+  const isVimeo = /vimeo\.com/.test(link)
+  const isYouTube = /youtu\.?be/.test(link)
+  if (!isVimeo && !isYouTube) {
+    return <p>Unsupported video URL</p>
+  }
+
   return (
-    // 16:9 aspect ratio wrapper
-    <div className=" relative w-full pb-[56.25%]">
+    <div className="relative w-full pb-[56.25%]">
       <ReactPlayer
         url={link}
         width="100%"
         height="100%"
         className="absolute top-0 left-0"
         controls
+        // Pass Vimeo-specific options here:
+        config={{
+          vimeo: {
+            playerOptions: {
+              // hide byline, title, portrait if you like:
+              byline: false,
+              portrait: false,
+              title: false,
+            }
+          }
+        }}
       />
     </div>
   )
